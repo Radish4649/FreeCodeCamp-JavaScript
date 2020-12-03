8bde2083b411981ff90499d62ecbcfd03d8c3937
Radish4649 <69351166+Radish4649@users.noreply.github.com>
2020-12-04 08:49:05 +0900

    Update convertToRoman.js
    updated readability

--- convertToRoman.js
+++ convertToRoman.js
@@ -1,16 +1,16 @@
 function convertToRoman(num, t=0) {
- 	let arr = ["","a", "aa", "aaa", 
-               "ab", "b", "ba", 
-               "baa", "baaa", "ac"]
+	let arr = ["","a", "aa", "aaa", 
+		   "ab", "b", "ba", 
+		   "baa", "baaa", "ac"]
 	let key = [["I", "V", "X"], 
-             ["X", "L", "C"], 
-             ["C", "D", "M"], 
-             ["M"]]
+		   ["X", "L", "C"], 
+		   ["C", "D", "M"], 
+		   ["M"]]
 	let modTen = num%10
 	let thisDigit = arr[modTen]
-                  .replace(/a/g, key[t][0])
-									.replace(/b/g, key[t][1])
-									.replace(/c/g, key[t][2])
+			.replace(/a/g, key[t][0])
+			.replace(/b/g, key[t][1])
+			.replace(/c/g, key[t][2])
 	if (num === num%10) {
 		return thisDigit
 	}
