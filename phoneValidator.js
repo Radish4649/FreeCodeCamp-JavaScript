119af7954987424745a5879306e4dc64a723dd57
Radish4649 <69351166+Radish4649@users.noreply.github.com>
2020-12-04 08:53:48 +0900

    Update phoneValidator.js

--- phoneValidator.js
+++ phoneValidator.js
@@ -1,17 +1,18 @@
 function telephoneCheck(str) {
-  let s = str.replace(/ /g, "")
-  let reg10 = /^\d{3}-\d{3}-\d{4}$/
+ 	let s = str.replace(/ /g, "")
+	let reg10 = /^\d{3}-\d{3}-\d{4}$/
 	let reg20 = /^\(\d{3}\)\d{3}-\d{4}$/
 	let reg30 = /^\d{10}$/
 	let reg11 = /^1\d{3}-\d{3}-\d{4}$/
 	let reg21 = /^1\(\d{3}\)\d{3}-\d{4}$/
 	let reg31 = /^1\d{10}/
+	
 	let arr = [reg10,reg20,reg30,reg11,reg21,reg31]
 	
 	for (let i = 0; i < 6; i++) {
-			if (arr[i].test(s)) {
-				return true
-			}
+		if (arr[i].test(s)) {
+			return true
+		}
 	}
-  return false
+	return false
 }
